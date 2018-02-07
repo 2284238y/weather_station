75251e7cfbad0154307626c8597aff6504a45b37
BeardedScotsman <craigmccallum.eng@gmail.com>
2018-02-07 19:40:58 +0000

    Isolated functions from threading

--- 02_data_processing/cpp_threads.cpp
+++ 02_data_processing/cpp_threads.cpp
@@ -1,52 +1,31 @@
 #include <iostream>
+#include <fstream>
+#include <fcntl.h>
+#include <sys/ioctl.h>
 #include <linux/i2c-dev.h>
 #include <pthread.h>
 #include "cpp_threads.h"
 
-using namespace std;
-
-int CppThreads::num = 0;
-
-CppThreads::CppThreads(){
-    id = num++;
-    cout << id << endl;
-}
-
-void CppThreads::Create(){
-    pthread_create(&id, 0, &CppThreads::Exec, this);
-}
-
-void* CppThreads::Exec(void* arg){
-    ((CppThreads*)arg)->CppThreads::Run();
-    return 0;
-}
-
-void CppThreads::Run() {
-    if (id == 0) {
-        int file_i2c;
-        unsigned char buffer[60];
-        int length = 2;
-
-        char *filename = "/dev/i2c-9";
-        if ((file_i2c = open(filename, 0_RDWR)) < 0) {
-            printf("Failed to open the i2c bus");
-            return;
-        }
-
-        int address = ;
-        if
-
-
-
-    } else if (id == 1){
-        cout << "Processing Data" << endl;
-    } else if (id == 2){
-        cout << "Broadcasting Data" << endl;
-    } else if (id == 3){
-        cout << "Updating GUI" << endl;
-    }
-}
-
-void CppThreads::Join(){
-    pthread_join(id, NULL);
-}
+// using namespace std;
+//
+// int CppThreads::num = 0;
+//
+// CppThreads::CppThreads(){
+//     id = num++;
+//     cout << id << endl;
+// }
+//
+// void CppThreads::Create(){
+//     pthread_create(&id, 0, &CppThreads::Exec, this);
+// }
+//
+// void* CppThreads::Exec(void* arg){
+//     ((CppThreads*)arg)->CppThreads::Run();
+//     return 0;
+// }
+//
+// void CppThreads::Run() {
+//
+// void CppThreads::Join(){
+//     pthread_join(id, NULL);
+// }
--- 02_data_processing/cpp_threads.h
+++ 02_data_processing/cpp_threads.h
@@ -1,17 +1,17 @@
 #ifndef CPP_THREADS_H
 #define CPP_THREADS_H
 
-class CppThreads{
-    public:
-        CppThreads();
-        virtual ~CppThreads(){};
-        void Create();
-        static void* Exec(void* arg);
-        void Run();
-        void Join();
-    private:
-        static int num;
-        pthread_t id;
-};
+// class CppThreads{
+//     public:
+//         CppThreads();
+//         virtual ~CppThreads(){};
+//         void Create();
+//         static void* Exec(void* arg);
+//         void Run();
+//         void Join();
+//     private:
+//         static int num;
+//         pthread_t id;
+// };
 
 #endif // CPP_THREADS_H
