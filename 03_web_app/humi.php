2e7344a8d4560c5b7d216fd151c8c2977a941909
Damir <35768349+damir2020@users.noreply.github.com>
2018-03-26 01:03:06 +0100

    Add files via upload

---
+++ 03_web_app/humi.php
@@ -0,0 +1,105 @@
+<?php
+//index.php
+$connect = mysqli_connect("localhost", "root", "EmbeddedSystemTeam2", "sensor_log");
+
+ 
+  $query = '
+  SELECT sensor_value, UNIX_TIMESTAMP(timestamp) AS datetime  
+  FROM table_sensor_log 
+  WHERE UNIX_TIMESTAMP(timestamp)>= '.time().'-24070 AND sensor_id = 2
+  ORDER BY timestamp ASC
+  LIMIT 600
+  '; 
+ 
+
+$result = mysqli_query($connect, $query);
+$rows = array();
+$table = array();
+
+$table['cols'] = array(
+ array(
+  'label' => 'Date Time', 
+  'type' => 'datetime'
+ ),
+ array(
+  'label' => 'Humidity (%)', 
+  'type' => 'number'
+ )
+);
+
+while($row = mysqli_fetch_array($result))
+{
+ $sub_array = array();
+ $datetime = explode(".", $row["datetime"]);
+ $sub_array[] =  array(
+      "v" => 'Date(' . $datetime[0] . '000)'
+     );
+ $sub_array[] =  array(
+      "v" => $row["sensor_value"]
+     );
+ $rows[] =  array(
+     "c" => $sub_array
+    );
+}
+$table['rows'] = $rows;
+$jsonTable = json_encode($table);
+
+if($_POST['timestamp']!=''){
+  header('Content-type:application/json');
+  echo $jsonTable;
+  exit();
+ }
+
+?>
+
+
+<html>
+ <head>
+  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+  <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+  <script type="text/javascript">
+   google.charts.load('current', {'packages':['corechart']});
+   google.charts.setOnLoadCallback(drawChart);
+   function drawChart()
+   {
+    jsonTable = <?php echo $jsonTable;?>;
+    window.data = new google.visualization.DataTable(<?php echo $jsonTable; ?>);
+
+    window.options = {
+     title:'Humidity, %',
+     legend:{position:'bottom'},
+     chartArea:{width:'95%', height:'65%'}
+    };
+
+    window.chart = new google.visualization.LineChart(document.getElementById('line_chart'));
+
+    chart.draw(data, options);
+   }
+
+   function updateChart(jsonTable){
+    data = new google.visualization.DataTable(jsonTable);
+ 
+    chart.draw(data, options);
+   }
+    setInterval(function(){
+       $.post('',{'timestamp':2},function(jT){ updateChart(jT);},'json');
+    },1000);
+    
+  </script>
+  <style>
+  .page-wrapper
+  {
+   width:1000px;
+   margin:0 auto;
+  }
+  </style>
+ </head>  
+ <body>
+  <div class="page-wrapper">
+   <br />
+   <h2 align="center">Display Google Line Chart with JSON PHP & Mysql</h2>
+   <div id="line_chart" style="width: 100%; height: 500px"></div>
+   <a href="http://192.168.0.23/test/index.php">Temperature link</a>
+  </div>
+ </body>
+</html>
